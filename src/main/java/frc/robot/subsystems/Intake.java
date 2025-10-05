4acdd64e7a78c4b52b14d717ba57663e7c91eaa1
sohan-bhat <sohanrambhat@gmail.com>
2025-10-04 20:20:31 -0500

    fixed bad code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -15,163 +15,165 @@ import frc.robot.Constants;
 import frc.robot.Constants.Intake.PivotSetpoints;
 
 public class Intake extends SubsystemBase {
-  private SparkFlex m_pivot =
-      new SparkFlex(Constants.Intake.kPivotMotorCanId, MotorType.kBrushless);
-  private AbsoluteEncoder m_pivotEncoder = m_pivot.getAbsoluteEncoder();
-  private SparkClosedLoopController m_pivotController = m_pivot.getClosedLoopController();
+    private SparkFlex m_pivot = new SparkFlex(Constants.Intake.kPivotMotorCanId, MotorType.kBrushless);
+    private AbsoluteEncoder m_pivotEncoder = m_pivot.getAbsoluteEncoder();
+    private SparkClosedLoopController m_pivotController = m_pivot.getClosedLoopController();
 
-  private SparkFlex m_roller =
-      new SparkFlex(Constants.Intake.kRollerMotorCanId, MotorType.kBrushless);
-  private SparkFlex m_indexer =
-      new SparkFlex(Constants.Intake.kIndexerMotorCanId, MotorType.kBrushless);
+    private SparkFlex m_roller = new SparkFlex(Constants.Intake.kRollerMotorCanId, MotorType.kBrushless);
+    private SparkFlex m_indexer = new SparkFlex(Constants.Intake.kIndexerMotorCanId, MotorType.kBrushless);
 
-  private SparkLimitSwitch m_outerBeamBreak = m_indexer.getReverseLimitSwitch();
-  private SparkLimitSwitch m_innerBeamBreak = m_indexer.getForwardLimitSwitch();
+    private SparkLimitSwitch m_outerBeamBreak = m_indexer.getReverseLimitSwitch();
+    private SparkLimitSwitch m_innerBeamBreak = m_indexer.getForwardLimitSwitch();
 
-  private enum PivotSetpoint {
-    STOW,
-    INTAKE,
-    EXTAKE,
-    SCORE
-  }
-
-  private enum RollerSetpoint {
-    INTAKE,
-    INTAKE_SLOW,
-    EXTATE,
-    SCORE,
-    STOP
-  }
-
-  public Intake() {
-    m_pivot.configure(
-        Configs.Intake.pivotConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_roller.configure(
-        Configs.Intake.rollerConfig,
-        ResetMode.kResetSafeParameters,
-        PersistMode.kPersistParameters);
-    m_indexer.configure(
-        Configs.Intake.indexerConfig,
-        ResetMode.kResetSafeParameters,
-        PersistMode.kPersistParameters);
-  }
-
-  /** Set the position of the pivot motor. */
-  private void setPivot(PivotSetpoint setpoint) {
-    double targetPosition = 0;
-    switch (setpoint) {
-      case STOW:
-        targetPosition = PivotSetpoints.kStow;
-        break;
-      case INTAKE:
-        targetPosition = PivotSetpoints.kIntake;
-        break;
-      case EXTAKE:
-        targetPosition = PivotSetpoints.kExtake;
-        break;
-      case SCORE:
-        targetPosition = PivotSetpoints.kScore;
-        break;
-      default:
-        return;
+    private enum PivotSetpoint {
+        STOW,
+        INTAKE,
+        EXTAKE,
+        SCORE
     }
-    m_pivotController.setReference(targetPosition, ControlType.kPosition);
-  }
 
-  /** Set the speed of the roller motor. */
-  private void setRollerSpeed(RollerSetpoint setpoint) {
-    double speed;
-    switch (setpoint) {
-      case INTAKE:
-        speed = Constants.Intake.RollerSetpoints.kIntake;
-        break;
-      case INTAKE_SLOW:
-        speed = Constants.Intake.RollerSetpoints.kIntakeSlow;
-        break;
-      case EXTATE:
-        speed = Constants.Intake.RollerSetpoints.kExtake;
-        break;
-      case SCORE:
-        speed = Constants.Intake.RollerSetpoints.kScore;
-        break;
-      case STOP:
-        speed = Constants.Intake.RollerSetpoints.kStop;
-        break;
-      default:
-        return;
+    private enum RollerSetpoint {
+        INTAKE,
+        INTAKE_SLOW,
+        EXTATE,
+        SCORE,
+        STOP
     }
-    m_roller.set(speed);
-  }
 
-  /** Set the speed of the indexer motor. */
-  private void setIndexerSpeed(RollerSetpoint setpiont) {
-    double speed;
-    switch (setpiont) {
-      case INTAKE:
-        speed = Constants.Intake.RollerSetpoints.kIntake;
-        break;
-      case INTAKE_SLOW:
-        speed = Constants.Intake.RollerSetpoints.kIntakeSlow;
-        break;
-      case EXTATE:
-        speed = Constants.Intake.RollerSetpoints.kExtake;
-        break;
-      case SCORE:
-        speed = Constants.Intake.RollerSetpoints.kScore;
-        break;
-      case STOP:
-        speed = Constants.Intake.RollerSetpoints.kStop;
-        break;
-      default:
-        return;
+    public Intake() {
+        m_pivot.configure(
+                Configs.Intake.pivotConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        m_roller.configure(
+                Configs.Intake.rollerConfig,
+                ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+        m_indexer.configure(
+                Configs.Intake.indexerConfig,
+                ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
     }
-    m_indexer.set(speed);
-  }
 
-  /**
-   * Intake the game piece and hold it when it is done to prepare for the next possible command
-   * which is either scoring or extaking.
-   */
-  public Command intakeCommand() {
-    return this.run(() -> {
-        setPivot(PivotSetpoint.INTAKE);
-        setRollerSpeed(RollerSetpoint.INTAKE);
-        setIndexerSpeed(RollerSetpoint.INTAKE);
-    }).until(() -> m_outerBeamBreak.isPressed()).andThen(() -> {
-        setRollerSpeed(RollerSetpoint.INTAKE_SLOW);
-        setIndexerSpeed(RollerSetpoint.INTAKE_SLOW);
-    }).until(() -> m_innerBeamBreak.isPressed())
-    .andThen(() -> {
-        setRollerSpeed(RollerSetpoint.STOP);
-        setIndexerSpeed(RollerSetpoint.STOP);
-    });
-    
-  }
+    /** Set the position of the pivot motor. */
+    private void setPivot(PivotSetpoint setpoint) {
+        double targetPosition = 0;
+        switch (setpoint) {
+            case STOW:
+                targetPosition = PivotSetpoints.kStow;
+                break;
+            case INTAKE:
+                targetPosition = PivotSetpoints.kIntake;
+                break;
+            case EXTAKE:
+                targetPosition = PivotSetpoints.kExtake;
+                break;
+            case SCORE:
+                targetPosition = PivotSetpoints.kScore;
+                break;
+            default:
+                return;
+        }
+        m_pivotController.setReference(targetPosition, ControlType.kPosition);
+    }
 
-  /** Spit the game piece back out. */
-  public Command extakeCommand() {
-    return this.run(() -> {
-        setPivot(PivotSetpoint.EXTAKE);
-        setRollerSpeed(RollerSetpoint.EXTATE);
-        setIndexerSpeed(RollerSetpoint.INTAKE);
-    });
-  }
+    /** Set the speed of the roller motor. */
+    private void setRollerSpeed(RollerSetpoint setpoint) {
+        double speed;
+        switch (setpoint) {
+            case INTAKE:
+                speed = Constants.Intake.RollerSetpoints.kIntake;
+                break;
+            case INTAKE_SLOW:
+                speed = Constants.Intake.RollerSetpoints.kIntakeSlow;
+                break;
+            case EXTATE:
+                speed = Constants.Intake.RollerSetpoints.kExtake;
+                break;
+            case SCORE:
+                speed = Constants.Intake.RollerSetpoints.kScore;
+                break;
+            case STOP:
+                speed = Constants.Intake.RollerSetpoints.kStop;
+                break;
+            default:
+                return;
+        }
+        m_roller.set(speed);
+    }
 
-  /** Intake the game piece further through the robot. */
-  public Command scoreCommand() {
-    return this.run(() -> {
-        setPivot(PivotSetpoint.SCORE);
-        setRollerSpeed(RollerSetpoint.SCORE);
-        setIndexerSpeed(RollerSetpoint.SCORE);
-    });
-  }
+    /** Set the speed of the indexer motor. */
+    private void setIndexerSpeed(RollerSetpoint setpiont) {
+        double speed;
+        switch (setpiont) {
+            case INTAKE:
+                speed = Constants.Intake.RollerSetpoints.kIntake;
+                break;
+            case INTAKE_SLOW:
+                speed = Constants.Intake.RollerSetpoints.kIntakeSlow;
+                break;
+            case EXTATE:
+                speed = Constants.Intake.RollerSetpoints.kExtake;
+                break;
+            case SCORE:
+                speed = Constants.Intake.RollerSetpoints.kScore;
+                break;
+            case STOP:
+                speed = Constants.Intake.RollerSetpoints.kStop;
+                break;
+            default:
+                return;
+        }
+        m_indexer.set(speed);
+    }
 
-  /** Stop the rollers and put in the stow position. */
-  public Command stowCommand() {
-    return this.run(() -> {
-        setPivot(PivotSetpoint.STOW);
-        setRollerSpeed(RollerSetpoint.STOP);
-        setIndexerSpeed(RollerSetpoint.STOP);
-    });
-  }
+    /**
+     * Intake the game piece and hold it when it is done to prepare for the next
+     * possible command
+     * which is either scoring or extaking.
+     */
+    public Command intakeCommand() {
+        return this.run(() -> {
+            setPivot(PivotSetpoint.INTAKE);
+            setRollerSpeed(RollerSetpoint.INTAKE);
+            setIndexerSpeed(RollerSetpoint.INTAKE);
+        }).until(() -> m_outerBeamBreak.isPressed()).andThen(() -> {
+            setRollerSpeed(RollerSetpoint.INTAKE_SLOW);
+            setIndexerSpeed(RollerSetpoint.INTAKE_SLOW);
+        }).until(() -> m_innerBeamBreak.isPressed())
+                .andThen(() -> {
+                    setRollerSpeed(RollerSetpoint.STOP);
+                    setIndexerSpeed(RollerSetpoint.STOP);
+                });
+
+    }
+
+    /** Spit the game piece back out. */
+    public Command extakeCommand() {
+        return this.run(() -> {
+            setPivot(PivotSetpoint.EXTAKE);
+            setRollerSpeed(RollerSetpoint.EXTATE);
+            setIndexerSpeed(RollerSetpoint.EXTATE);
+        })
+    }
+
+    /** Intake the game piece further through the robot. */
+    public Command scoreCommand() {
+        return this.run(() -> {
+            setPivot(PivotSetpoint.SCORE);
+            setRollerSpeed(RollerSetpoint.SCORE);
+            setIndexerSpeed(RollerSetpoint.SCORE);
+        }).until(() -> !m_innerBeamBreak.isPressed())
+        .andThen(() -> {
+            setRollerSpeed(RollerSetpoint.STOP);
+            setIndexerSpeed(RollerSetpoint.STOP);
+        });
+    }
+
+    /** Stop the rollers and put in the stow position. */
+    public Command stowCommand() {
+        return this.run(() -> {
+            setPivot(PivotSetpoint.STOW);
+            setRollerSpeed(RollerSetpoint.STOP);
+            setIndexerSpeed(RollerSetpoint.STOP);
+        });
+    }
 }
